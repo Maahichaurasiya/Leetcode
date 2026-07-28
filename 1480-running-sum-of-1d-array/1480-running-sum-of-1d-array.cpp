7b02e2efc7557f586cb6958cdf453682d6b9fe37
Maahi Chaurasiya <2k24.csj1.2410945@gmail.com>
2026-07-28 14:25:41 +0530

    Time: 0 ms (100%), Space: 12.6 MB (53.76%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,14 +1,44 @@
+/*
+Approach:
+---------
+We need to find the running sum of the array.
 
+The running sum means that every element stores the sum of itself
+and all the previous elements.
+
+Instead of calculating the sum from the beginning every time,
+we keep a variable `sum` that stores the running total..
+
+Algorithm:
+----------
+1. Create an empty vector `ans`.
+2. Initialize `sum = 0`.
+3. Traverse the array from left to right.
+4. Add the current element to `sum`.
+5. Push `sum` into the answer vector.
+6. Return the answer vector.
+
+
+
+Time Complexity:
+----------------
+O(n)
+We traverse the array only once.
+
+Space Complexity:
+-----------------
+O(n)
+An extra vector is used to store the running sums.
+*/
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int>ans;
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+        vector<int> ans;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
             ans.push_back(sum);
         }
         return ans;
-        
     }
 };
