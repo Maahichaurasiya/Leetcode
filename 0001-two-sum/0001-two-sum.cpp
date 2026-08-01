e6dd0b54acbebad87a305ff5f4b94de56a7a63d2
Maahi Chaurasiya <2k24.csj1.2410945@gmail.com>
2026-08-01 11:10:59 +0530

    Time: 0 ms (100%), Space: 14.9 MB (39.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,39 +1,67 @@
-/*
-Approach: Brute Force
+
+    /*
+
+    Approach: Better (Sorting + Two Pointers)
+
+    Idea:
+    - The brute force approach checks every possible pair, which takes O(N²)
+    time.
+    - To improve this, store each element along with its original index.
+    - Sort the elements based on their values.
+    - Use two pointers:
+        • Left pointer starts from the beginning.
+        • Right pointer starts from the end.
+    - Compare the sum of the two elements:
+        • If the sum equals the target, return their original indices.
+        • If the sum is smaller than the target, move the left pointer forward.
+        • If the sum is greater than the target, move the right pointer backward
 
 
-Algorithm:
-1. Traverse the array using index 'i'.
-2. For each element, traverse the remaining elements using index 'j = i + 1'.
-3. Check if:
-       nums[i] + nums[j] == target
-4. If true, return {i, j}.
-5. If no valid pair is found, return an empty vector.
+    Algorithm:
+    1. Store each element with its original index.
+    2. Sort the vector according to the values.
+    3. Initialize two pointers:
+          i = 0
+          j = n - 1
+    4. While i < j:
+          - If sum == target, return the original indices.
+          - If sum < target, move i++.
+          - Otherwise, move j--.
+    5. If no valid pair is found, return an empty vector.
 
+    Time Complexity:
+    - Sorting: O(N log N)
+    - Two Pointer Traversal: O(N)
+    - Overall: O(N log N)
 
-Time Complexity:
-- O(N²)
-  (Two nested loops check every possible pair.)
+    Space Complexity:
+    - O(N)
+      (Extra space is used to store value-index pairs.)
+ */
 
-Space Complexity:
-- O(1)
-  (No extra data structure is used.)
-
-*/
-
-
-class Solution {
+    class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
+        vector<pair<int, int>> ans;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            ans.push_back({nums[i], i});
+        }
+        sort(ans.begin(), ans.end());
+
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int sum = ans[left].first + ans[right].first;
+            if (sum == target) {
+                return {ans[left].second, ans[right].second};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-        
+
         return {};
     }
 };
