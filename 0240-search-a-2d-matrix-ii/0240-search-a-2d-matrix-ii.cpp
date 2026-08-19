308ccec28ebf8b2928265878f1c6c7db75b59472
Maahi Chaurasiya <2k24.csj1.2410945@gmail.com>
2026-08-19 20:50:06 +0530

    Time: 679 ms (5.18%), Space: 18.6 MB (69.2%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
